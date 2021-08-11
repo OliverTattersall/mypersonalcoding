3a6274e5a6439bba169caa7709c970d891c2d160
OliverTattersall <oliver.tattersall@ucc.on.ca>
2021-08-11 17:38:15 -0400

    first ai

---
+++ learning AI/breastcancerAIlearn/run.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+
+
+dataset = pd.read_csv('cancer.csv')
+
+
+x = dataset.drop(columns=['diagnosis(1=m, 0=b)'])
+y = dataset['diagnosis(1=m, 0=b)']
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+
+model = tf.keras.models.load_model("breastcancerai.h5")
+
+
+
+evaled = model.evaluate(x_test, y_test)
